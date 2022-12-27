0f772dcceb3d9bae73f49f842404d2864cf3801e
von <von@motionz.tech>
2022-12-27 17:45:26 +0800

    refactor: scroll by mouse on desktop

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,20 @@
+import 'dart:ui';
+
 import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        // etc.
+      };
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior().copyWith(scrollbars: false),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
